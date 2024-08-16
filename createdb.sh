865d77cf97f27daa654677442e869218a40b079a
rogeriosilva <rogerio.silva@ctw.bmwgroup.com>
2024-08-16 12:04:09 +0100

    Starting creating scripts

---
+++ createdb.sh
@@ -0,0 +1,5 @@
+# Create the AI db for The Urantia book
+# Script for windows
+# The environment for python should be already created using: python -m venv tub_ai
+echo "Activating the environment"
+source tub_ai/Scripts/activate
